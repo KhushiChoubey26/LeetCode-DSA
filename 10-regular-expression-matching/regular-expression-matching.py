986baf76f6bfaa67d65c7897eda57255ae6f3fc4
Khushi Choubey <khushichoubey19@gmail.com>
2025-06-28 00:33:19 +1000

    Time: 8 ms (84.02%) | Memory: 12.5 MB (30.90%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        m, n = len(s), len(p)
+        
+        # dp[i][j] will be True if s[:i] matches p[:j]
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True  # empty string matches empty pattern
+
+        # Handle patterns like a*, a*b*, a*b*c* that can match empty string
+        for j in range(2, n + 1):
+            if p[j - 1] == '*':
+                dp[0][j] = dp[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    # Match zero occurrences
+                    dp[i][j] = dp[i][j - 2]
+                    # Match one or more if preceding matches
+                    if p[j - 2] == '.' or p[j - 2] == s[i - 1]:
+                        dp[i][j] |= dp[i - 1][j]
+        
+        return dp[m][n]
